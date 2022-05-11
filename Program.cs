9875d0916ab4f961424b0a7a9f30427bb87cf79c
Arun Kumar <arunmech1997ak@gmail.com>
2022-05-11 20:43:29 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 3.75;
+            double area = CircleArea(radius);
+            Console.WriteLine("Area is {0}",area);
+        }
+        public static double CircleArea(double r)
+        {
+            double pi = System.Math.PI;
+            double area = pi * r * r;
+            return area;
+        }
+    }
+}
